c281cf7ea5f812777ab716a96554f2acd0c73584
Steven Wobser <steven@nofox.io>
2023-08-16 20:32:46 +0200

    Add simple logging

--- libmachine/log/fmt_machine_logger.go
+++ libmachine/log/fmt_machine_logger.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
@@ -11,6 +12,31 @@ type FmtMachineLogger struct {
 	errWriter io.Writer
 	debug     bool
 	history   *HistoryRecorder
+	logFile   *os.File
+}
+
+func openLogFile() *os.File {
+	homeDir, homeErr := os.UserHomeDir()
+	if homeErr != nil {
+		log.Fatalf("Could not get user home directory: %s", homeErr.Error())
+	}
+
+	machineDir := fmt.Sprintf("%s/.docker/machine", homeDir)
+	_, statErr := os.Stat(machineDir)
+	if os.IsNotExist(statErr) {
+		createErr := os.MkdirAll(machineDir, 0700)
+		if createErr != nil {
+			log.Fatalf("Could not create machine directory: %s", createErr.Error())
+		}
+	}
+
+	file, err := os.OpenFile(fmt.Sprintf("%s/docker-machine.log", machineDir), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatalf("Could not open log file: %s", err.Error())
+	}
+
+	return file
 }
 
 // NewFmtMachineLogger creates a MachineLogger implementation used by the drivers
@@ -20,6 +46,7 @@ func NewFmtMachineLogger() MachineLogger {
 		errWriter: os.Stderr,
 		debug:     false,
 		history:   NewHistoryRecorder(),
+		logFile:   openLogFile(),
 	}
 }
 
@@ -35,48 +62,66 @@ func (ml *FmtMachineLogger) SetErrWriter(err io.Writer) {
 	ml.errWriter = err
 }
 
+func (ml *FmtMachineLogger) writeErr(str string) {
+	ml.logFile.WriteString(str)
+	fmt.Fprint(ml.errWriter, str)
+}
+
+func (ml *FmtMachineLogger) writeOut(str string) {
+	ml.logFile.WriteString(str)
+	fmt.Fprint(ml.outWriter, str)
+}
+
 func (ml *FmtMachineLogger) Debug(args ...interface{}) {
 	ml.history.Record(args...)
 	if ml.debug {
-		fmt.Fprintln(ml.errWriter, args...)
+		str := fmt.Sprintln(args...)
+		ml.writeErr(str)
 	}
 }
 
 func (ml *FmtMachineLogger) Debugf(fmtString string, args ...interface{}) {
 	ml.history.Recordf(fmtString, args...)
 	if ml.debug {
-		fmt.Fprintf(ml.errWriter, fmtString+"\n", args...)
+		str := fmt.Sprintf(fmtString+"\n", args...)
+		ml.writeErr(str)
 	}
 }
 
 func (ml *FmtMachineLogger) Error(args ...interface{}) {
 	ml.history.Record(args...)
-	fmt.Fprintln(ml.errWriter, args...)
+	str := fmt.Sprintln(args...)
+	ml.writeErr(str)
 }
 
 func (ml *FmtMachineLogger) Errorf(fmtString string, args ...interface{}) {
 	ml.history.Recordf(fmtString, args...)
-	fmt.Fprintf(ml.errWriter, fmtString+"\n", args...)
+	str := fmt.Sprintf(fmtString+"\n", args...)
+	ml.writeErr(str)
 }
 
 func (ml *FmtMachineLogger) Info(args ...interface{}) {
 	ml.history.Record(args...)
-	fmt.Fprintln(ml.outWriter, args...)
+	str := fmt.Sprintln(args...)
+	ml.writeOut(str)
 }
 
 func (ml *FmtMachineLogger) Infof(fmtString string, args ...interface{}) {
 	ml.history.Recordf(fmtString, args...)
-	fmt.Fprintf(ml.outWriter, fmtString+"\n", args...)
+	str := fmt.Sprintf(fmtString+"\n", args...)
+	ml.writeOut(str)
 }
 
 func (ml *FmtMachineLogger) Warn(args ...interface{}) {
 	ml.history.Record(args...)
-	fmt.Fprintln(ml.outWriter, args...)
+	str := fmt.Sprintln(args...)
+	ml.writeOut(str)
 }
 
 func (ml *FmtMachineLogger) Warnf(fmtString string, args ...interface{}) {
 	ml.history.Recordf(fmtString, args...)
-	fmt.Fprintf(ml.outWriter, fmtString+"\n", args...)
+	str := fmt.Sprintf(fmtString+"\n", args...)
+	ml.writeOut(str)
 }
 
 func (ml *FmtMachineLogger) History() []string {
